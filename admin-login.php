9becd1a498c7bb28720c351ddc58767fa72afdb1
Parth joshi <parthjoshi@Parths-MacBook-Air.local>
2023-11-01 15:55:35 -0700

    Secure section added

---
+++ admin-login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Admin Login</title>
+</head>
+<body>
+    <h2>Admin Login</h2>
+    <form action="login.php" method="post">
+        <label for="username">Username:</label>
+        <input type="text" name="username" required><br>
+        <label for="password">Password:</label>
+        <input type="password" name="password" required><br>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
---
+++ logout.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+// Destroy the session to log out the admin
+session_destroy();
+header("Location: admin_login.php");
+exit();
+?>
