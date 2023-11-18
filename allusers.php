35014ce8cea83a28039a580cd641f013f34749d3
Parth joshi <parthjoshi@Parths-MacBook-Air.local>
2023-11-17 21:46:01 -0800

    Updated till CURL LAB

--- allusers.php
+++ allusers.php
@@ -7,37 +7,42 @@ $myArr = array(
     "Biden",
 );
 
-// Fetch data from the remote URL
+$remote_urls = [ 'https://peaceful-crag-29178-e57028cb57c2.herokuapp.com/users.php','http://hritik.us.to/cmpe-272-gym/users.php'];
+
 $remoteData = array();
+foreach ($remote_urls as $remote_url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $remote_url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'http://hritik.us.to/cmpe-272-gym/users.php');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$response = curl_exec($ch);
+    if (curl_errno($ch)) {
+        echo 'cURL Error: ' . curl_error($ch);
+    }
 
-if (curl_errno($ch)) {
-    echo 'cURL Error: ' . curl_error($ch);
-}
+    curl_close($ch);
 
-curl_close($ch);
-
-if ($response === false) {
-    echo 'cURL request failed';
-} else {
-    $remoteData = json_decode($response);
-    if ($remoteData === null) {
-        echo 'JSON parsing failed';
+    if ($response === false) {
+        echo 'cURL request failed';
+    } else {
+        $decodedData = json_decode($response);
+        if ($decodedData === null) {
+            echo 'JSON parsing failed';
+        } else {
+            $remoteData = array_merge($remoteData, $decodedData);
+        }
     }
 }
 
-// Merge local and remote data
 $mergedData = array_merge($myArr, $remoteData);
 
-// Display the merged data
 echo '<h1>All User Data</h1>';
 echo '<ul>';
+
 foreach ($mergedData as $user) {
     echo '<li>' . htmlspecialchars($user) . '</li>';
 }
+
 echo '</ul>';
 ?>
+
